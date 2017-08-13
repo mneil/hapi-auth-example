69e4514a1cf33ec6d5cb2c07e5f1de408b3c81d1
Michael Neil <mneil@mneilsworld.com>
2017-08-13 10:19:14 -0700

    Twitter auth
    Not getting email yet. Cookie authentication is setup
    
    [(Starts) #148849573]

--- auth/index.js
+++ auth/index.js
@@ -1,42 +1,75 @@
 const bell = require('bell');
 const config = require('config');
+const authCookie = require('hapi-auth-cookie');
+const Boom = require('boom');
 
 module.exports = function auth(server) {
-  // todo: auth
-  server.register(bell, (err) => {
+  // Register bell and hapi-auth-cookie with the server
+  server.register([authCookie, bell], (err) => {
     if (err) {
       throw err;
     }
-    // Declare an authentication strategy using the bell scheme
-    // with the name of the provider, cookie encryption password,
-    // and the OAuth client credentials.
+    // Setup the session strategy
+    server.auth.strategy('session', 'cookie', {
+      // cookie: 'abp',
+      password: config.get('cookie.secret'), // Use something more secure in production
+      redirectTo: '/login', // If there is no session, redirect here
+      isSecure: false, // Should be set to true (which is the default) in production
+      isSameSite: 'Lax',
+    });
+    server.auth.default('session');
+    // Setup the social Twitter login strategy
     server.auth.strategy('twitter', 'bell', {
       provider: 'twitter',
-      password: 'cookie_encryption_password_secure',
+      password: config.get('twitter.secret'),
       clientId: config.get('twitter.id'),
       clientSecret: config.get('twitter.secret'),
-      // Terrible idea but required if not using HTTPS especially if developing locally
-      isSecure: false,
+      isSecure: config.get('cookie.secure'), // Should be set to true (which is the default) in production
     });
-    // Use the 'twitter' authentication strategy to protect the
-    // endpoint handling the incoming authentication credentials.
-    // This endpoints usually looks up the third party account in
-    // the database and sets some application state (cookie) with
-    // the local application account information.
+    // Setup the routes (this could be done in an own file but for the sake of simplicity isn't)
     server.route({
-      method: ['GET', 'POST'], // Must handle both GET and POST
-      path: '/login', // The callback endpoint registered with the provider
+      method: 'GET',
+      path: '/auth/twitter',
       config: {
-        auth: 'twitter',
+        auth: 'twitter', // <-- use our twitter strategy and let bell take over
         handler: (request, reply) => {
           if (!request.auth.isAuthenticated) {
-            return reply(`Authentication failed due to: ${request.auth.error.message}`);
+            return reply(Boom.unauthorized(`Authentication failed due to: ${request.auth.error.message}`));
           }
-          // Perform any account lookup or registration, setup local session,
-          // and redirect to the application. The third-party credentials are
-          // stored in request.auth.credentials. Any query parameters from
-          // the initial request are passed back via request.auth.credentials.query.
-          return reply.redirect('/home');
+          // Just store a part of the twitter profile information in the session as an example. 
+          // You could do something more useful here 
+          // - like loading or setting up an account (social signup).
+          const profile = request.auth.credentials.profile;
+          request.cookieAuth.set({
+            twitterId: profile.id,
+            username: profile.username,
+            displayName: profile.displayName,
+          });
+          // console.log('doing a redirect', profile, profile.raw.entities);
+          return reply.redirect('/');
+        },
+      },
+    });
+    server.route({
+      method: 'GET',
+      path: '/',
+      config: {
+        auth: 'session', // <-- require a session for this, so we have access to the twitter profile
+        handler: (request, reply) => {
+          // Return a message using the information from the session
+          reply(`Hello, ${request.auth.credentials.displayName}!`);
+        },
+      },
+    });
+    server.route({
+      method: 'GET',
+      path: '/login',
+      config: {
+        auth: { mode: 'try' },
+        plugins: { 'hapi-auth-cookie': { redirectTo: false } },
+        handler: (request, reply) => {
+          // Return a message using the information from the session
+          reply('Hello, you need to login!<p><a href="/auth/twitter">Login with Twitter</a></p>');
         },
       },
     });
