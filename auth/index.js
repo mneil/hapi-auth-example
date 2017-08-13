0b7c19b701aad0d5ab08a30d161b38848a0703e0
Michael Neil <mneil@mneilsworld.com>
2017-08-13 16:10:11 -0700

    Fix missing session auth handler

--- auth/index.js
+++ auth/index.js
@@ -4,21 +4,24 @@ const Providers = require('./providers');
 const Routes = require('./routes');
 
 module.exports = function auth(server) {
-  // Register bell and hapi-auth-cookie with the server
-  server.register([AuthCookie, Bell], (err) => {
-    if (err) {
-      throw err;
-    }
-    // Add our authentication strategies
-    server.auth.strategy('session', 'cookie', Providers.cookie);
-    server.auth.strategy('twitter', 'bell', Providers.twitter);
-    server.auth.strategy('facebook', 'bell', Providers.facebook);
-    server.auth.strategy('google', 'bell', Providers.google);
-    // Default authentication strategy
-    server.auth.default('session');
+  return new Promise((resolve, reject) => {
+    // Register bell and hapi-auth-cookie with the server
+    server.register([AuthCookie, Bell], (err) => {
+      if (err) {
+        return reject(err);
+      }
+      // Add our authentication strategies
+      server.auth.strategy('session', 'cookie', Providers.cookie);
+      server.auth.strategy('twitter', 'bell', Providers.twitter);
+      server.auth.strategy('facebook', 'bell', Providers.facebook);
+      server.auth.strategy('google', 'bell', Providers.google);
+      // Default authentication strategy
+      server.auth.default('session');
 
-    server.route(Routes.twitter);
-    server.route(Routes.facebook);
-    server.route(Routes.google);
+      server.route(Routes.twitter);
+      server.route(Routes.facebook);
+      server.route(Routes.google);
+      return resolve();
+    });
   });
 };
--- server.js
+++ server.js
@@ -9,39 +9,38 @@ server.connection({
   port: config.get('port'),
 });
 
-auth(server);
-
-server.route({
-  method: 'GET',
-  path: '/',
-  config: {
-    auth: 'session', // require a session for this, so we have access to the twitter profile
-    handler: (request, reply) => {
-      // Return a message using the information from the session
-      reply(`Hello, ${request.auth.credentials.displayName}!`);
+auth(server).then(() => {
+  server.route({
+    method: 'GET',
+    path: '/',
+    config: {
+      auth: 'session', // require a session for this, so we have access to the twitter profile
+      handler: (request, reply) => {
+        // Return a message using the information from the session
+        reply(`Hello, ${request.auth.credentials.displayName}!`);
+      },
     },
-  },
-});
-server.route({
-  method: 'GET',
-  path: '/login',
-  config: {
-    auth: { mode: 'try' },
-    plugins: { 'hapi-auth-cookie': { redirectTo: false } },
-    handler: (request, reply) => {
-      // Return a message using the information from the session
-      reply(`<h2>Hello, you need to login!</h2/>
-      <p><a href="/auth/twitter">Login with Twitter</a></p>
-      <p><a href="/auth/facebook">Login with Facebook</a></p>
-      <p><a href="/auth/google">Login with Google</a></p>`);
+  });
+  server.route({
+    method: 'GET',
+    path: '/login',
+    config: {
+      auth: { mode: 'try' },
+      plugins: { 'hapi-auth-cookie': { redirectTo: false } },
+      handler: (request, reply) => {
+        // Return a message using the information from the session
+        reply(`<h2>Hello, you need to login!</h2/>
+        <p><a href="/auth/twitter">Login with Twitter</a></p>
+        <p><a href="/auth/facebook">Login with Facebook</a></p>
+        <p><a href="/auth/google">Login with Google</a></p>`);
+      },
     },
-  },
-});
-
-// Start the server
-server.start((err) => {
-  if (err) {
-    throw err;
-  }
-  console.log('Server running at:', server.info.uri);
+  });
+  // Start the server
+  server.start((err) => {
+    if (err) {
+      throw err;
+    }
+    server.log(['info'], `Server running at: ${server.info.uri}`);
+  });
 });
